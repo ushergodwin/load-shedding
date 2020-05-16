4069e8df01225da3b5e3ef9baafe553ea98e1900
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2020-05-16 12:49:13 +0100

    Update registerschedule.php

--- registerschedule.php
+++ registerschedule.php
@@ -10,69 +10,26 @@
 <link rel="stylesheet" type="text/css" href="register.css">
 <script src="shedding.js"> </script>
 <style type="text/css">
-.dropbtn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 16px;
-  font-size: 16px;
-  border: none;
-  cursor: pointer;
-}
-
-/* The container <div> - needed to position the dropdown content */
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
+.dropbtn { background-color: #4CAF50; color: white; padding: 16px;font-size: 16px; border: none; cursor: pointer;
+}/* The container <div> - needed to position the dropdown content */
+.dropdown {position: relative;display: inline-block;}
 /* Dropdown Content (Hidden by Default) */
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
+.dropdown-content {display: none;position: absolute;background-color: #f9f9f9;min-width: 160px;box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;}
 /* Links inside the dropdown */
-.dropdown-content a {
-  color: blue;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-}
-
+.dropdown-content a { color: blue;padding: 12px 16px;text-decoration: none;display: block;}
 /* Change color of dropdown links on hover */
 .dropdown-content a:hover {background-color: #f1f1f1}
-
 /* Show the dropdown menu on hover */
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
+.dropdown:hover .dropdown-content {display: block;}
 /* Change the background color of the dropdown button when the dropdown content is shown */
 .dropdown:hover .dropbtn {
   background-color: #3e8e41;
 }
-
-
- #column1 { 
-      background-color: #a1edcc; 
-     width: 350px;
-     
-      } 
-      #column2 { 
-      background-color:#a0e9ed; 
-     width: 400px;
-      } 
-      #column3 { 
-      background-color:#f497f1;
-     width: 320px;
-
-      } 
-      body{ background-image: url("imgs/20200426_115231_edited.jpg");
-  background-repeat: round; }
+ #column1 { background-color: #a1edcc; width: 350px; } 
+ #column2 { background-color:#a0e9ed; width: 400px; } 
+#column3 {  background-color:#f497f1;width: 320px;} 
+body{ background-image: url("imgs/20200426_115231_edited.jpg"); background-repeat: round; }
     </style>
 <script>
 var locationObject = {
