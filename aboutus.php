974507cdb77c5d4534ddd8cdf6964e4d99a1ea87
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2020-05-18 08:26:15 +0100

    Add files via upload
    added bootstrap cards, with rows that align the cards horizontally. however am considering using card-group which makes the card appear with the same height and width, something not achieved with rows

--- aboutus.php
+++ aboutus.php
@@ -1,3 +1,4 @@
+
 <html>
 <head> <script src="slideshow.js"></script>
 	<title>About Us - ugsheds</title>
@@ -14,38 +15,59 @@
 <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
     <link rel="icon" href="imgs/favicon.ico">
-    <style type="text/css">h5{color: blue}  html, body{ max-width: 100%; background-color: aliceblue; font-family: 'Roboto', sans-serif, serif;} div, li{ margin-left: none;} .usher{ float: left; width: 20%; max-height: 300px; padding: 10px;}  .footer{  background-color: #f5f5f5; width: auto; max-width: 100%; padding: 0 15px; }</style>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <style type="text/css">h5{color: blue}  html, body{ max-width: 100%; background-color: aliceblue; font-family: 'Roboto', sans-serif, serif;} div, li{ margin-left: none;} #usher{ float: left; width: 20%; max-height: 300px; padding: 10px;}  .footer{  background-color: #f5f5f5; width: auto; max-width: 100%; padding: 0 15px; }</style>
     </head>
 <body>
-    <div align="center">
-    <h1> About Us</h1>
-    </div>
- <h3>ugsheds is subdoim of cocis news designed by a group of students at Makerere Univeristy. The purpose of the system is to provide Uganda's power outages schedules to the users.</h3>  
+    <div align="center" class="card" style="width: 100%">
+    <h1 class="card-header"> About Us</h1>
+        <div class="card-body">
+ <h3 class="card-text">ugsheds is subdoim of cocis news designed by a group of students at Makerere Univeristy. The purpose of the system is to provide Uganda's power outages schedules to the users.</h3>  
         <h5>In case you need Information Systems ie including but not limited to, online stores both web and mobile application, network configuration set up and management, databse design and management, please contact any of the following Members</h5>
-        <div class="container">
-            <img src="imgs/usher.jpg" class="usher"> &nbsp; &nbsp;
-    <h4>TUMUHIMBISE USHER GODWIN</h4>
-        <p>He's a passionate and hardworking student at Makerere University, persuing a Bachelors Degree in Information Systems and Technology. His passion lies under Development and Networking</p>
+    </div>
+    <div class="row" style="padding: 30px">
+         <div class="col-md6">
+        <div class="card" style="width: 500px">
+            <img src="imgs/usher.jpg" class="card-img-top">
+            <div class="card-test-white bg-light">
+            <div class="card-body text-center">
+    <h4 class="card-title">TUMUHIMBISE USHER GODWIN</h4>
+        <p class="card-text">He's a passionate and hardworking student at Makerere University, persuing a Bachelors Degree in Information Systems and Technology. His passion lies under Development and Networking</p>
         <p>Contacts</p>
-      &nbsp; &nbsp;     <ol>
-       &nbsp; &nbsp;         <li>godwintumuhimbise96@gmail.com</li>
-       &nbsp; &nbsp;      <li>+256756809525 </li>
-        &nbsp; &nbsp;      <li>+256784664867</li>
+          <ol class="card-text">
+            <li>godwintumuhimbise96@gmail.com</li>
+            <li>+256756809525 </li>
+              <li>+256784664867</li>
         </ol>
+         <a href="#" class="btn btn-primary">View Profile</a>
+        </div> 
+            </div>            
         </div>
-    <div> 
-        <img src="imgs/flo.jpg" class="usher">
-         <h4>ALAROKER FLORENCE</h4>
-        <p>Alarocker Florence is a second year student pursuing Bachelor's Degree in Information Systems and Technology at Makerere University. She's a hardworking and detemined to do her best in everything she does</p>
-        <p>Contact</p>
-          <ol>
-              <li>+256751534654 </li>
-        </ol>
 </div>
-    
-    
-    <br> <br> <br> <br> <br> <br> <br> <br>  <hr> <br><br> <br> <br> <br> 
-<div class="footer" align="center">
+  <div class="col-md6" style="padding-left: 30px">
+     <div class="card"  style="width: 500px" >
+     <img src="imgs/flo.jpg" class="card-img-top">
+         <div class="card-text-white bg-secondary mb4">
+          <div class="card-body">
+            <h4 class="card-title">ALAROKER FLORENCE</h4>
+        <p class="card-text">Alarocker Florence is a second year student pursuing Bachelor's Degree in Information Systems and Technology at Makerere University. She's a hardworking and detemined to do her best in everything she does</p>
+        <p class="card-text">Contact</p>
+          <ol class="card-text">
+         <li>+256751534654 </li>
+        </ol>
+         <a href="#" class="btn btn-primary">View Profile</a>
+          </div>
+         </div>
+          </div>
+        </div>
+        
+</div> 
+<div class="card-footer text-muted">
     <table>
         <tr>
             <th colspan="2"> <p style="color: blue;"> Copyright &copy BIST Group C LoadShedding 2020 | All Rights Reserved</p></th> </tr>
@@ -65,5 +87,6 @@
       </tr>
   </table>
   </div>
+    </div>
 </body>
 </html>
