cb3ae3e4f54ec4969d1bb95a7eaa264ef4526094
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2020-05-20 14:33:26 +0100

    Update index.php
    excluded the 'anoying' fixed button. set the form in the navbar as inline

--- index.php
+++ index.php
@@ -22,40 +22,17 @@
 <style type="text/css">
     <style type="text/css"> .closebtn{margin-left: 15px; color: white; font-weight: bold; font-size: 22; line-height: 20px; cursor: hand; transition: 0.3s;} .closebtn:hover{color: yellow;} .alert{padding: 20px; background-color: #f44336; color: white; margin-bottom: 15px;} #alert{ display: none; max-width: 100% } a{ font-size: 15px;} #icon-bar{top: 50px; position: fixed; display: inline-block;margin-left: 20px; }
         
-        /* Button used to open the contact form - fixed at the bottom of the page */
-.open-button { background-color: #555; color: white; padding: 16px 20px; border: none; cursor: pointer; opacity: 0.8; position: fixed;bottom: 23px;right: 28px;width: 280px;}
 /* The popup form - hidden by default */
-.form-popup {display: none;position: fixed;bottom: 0;right: 15px;border: 3px solid #f1f1f1;z-index: 9;}
+.form-popup {display: none;position: fixed;bottom: 0;right: 15px;border: 2px solid blue;z-index: 9; border-radius: 5px; border-style: outset}
 /* Add styles to the form container */
-.form-container { max-width: 300px; padding: 10px; background-color: white;}
+.form-container { max-width: 500px; padding: 10px; background-color: white;}
 /* Full-width input fields */
-.form-container input[type=text], .form-container input[type=password] {  width: 100%; padding: 15px; margin: 5px 0 22px 0; border: none;
+.form-container input[type=text]{  width: 100%; padding: 15px; margin: 5px 0 22px 0; border: none;
   background: #f1f1f1;
 }
-/* When the inputs get focus, do something */
-.form-container input[type=text]:focus, .form-container input[type=password]:focus { background-color: #ddd; outline: none;}
-/* Set a style for the submit/login button */
-.form-container .btn { background-color: #4CAF50;color: white; padding: 16px 20px;border: none; cursor: pointer; width: 100%; margin-bottom:10px; opacity: 0.8;}
-/* Add a red background color to the cancel button */
-        .form-container .cancel { background-color: red;}
-/* Add some hover effects to buttons */
-.form-container .btn:hover, .open-button:hover {
-    opacity: 1;} body {font-family: 'Roboto', sans-serif; font-size: 18px;}  .style{ background-color: aliceblue} p{font-size: 18px;} a{  font-size: 18px;} td{ width: auto} /* Add a red background color to the cancel button */
-.form-container .cancel {
-  background-color: red;}
-/* Add some hover effects to buttons */
-.form-container .btn:hover, .open-button:hover {opacity: 1;}
-/* Full-width input fields */
-input[type=text], input[type=password] {
-width: 100%;padding: 12px 20px;margin: 8px 0;display: inline-block;border: 1px solid #ccc;box-sizing: border-box;
-}/* Set a style for all buttons */
-button { background-color: #4CAF50;color: white;padding: 14px 20px;margin: 8px 0;border: none;cursor: pointer;width: 100%;
-}button:hover {opacity: 0.8;}
+ body {font-family: 'Roboto', sans-serif; font-size: 18px;}  .style{ background-color: aliceblue} p{font-size: 18px;} a{  font-size: 18px;}
 /* Extra styles for the cancel button */
 .cancelbtn {width: auto;padding: 10px 18px;background-color: #f44336;}/* Center the image and position the close button */
-.imgcontainer {text-align: center;margin: 24px 0 12px 0;
-position: relative;}
-img.avatar { width: 40%;border-radius: 50%;}
 .container { padding: 16px;}
 span.psw {float: right;padding-top: 16px;}
 /* The Modal (background) */
@@ -73,12 +50,8 @@ span.psw {float: right;padding-top: 16px;}
 @keyframes animatezoom {from {transform: scale(0)} to {transform: scale(1)}}
 /* Change styles for span and cancel button on extra small screens */@media screen and (max-width: 300px) {span.text {
 display: block;float: none;}
-  .cancelbtn { width: 100%; }}  #sub{background-color: #4CAF50;color: white;border: none;cursor: pointer;width: 15%;
-} table tr td, table tr th {padding:0.625rem;}
-table tfoot, table thead,table tr:nth-of-type(2n) {background:none repeat scroll 0 0 #f0f0f0;}
-th,table tr:nth-of-type(2n) td {border-right:1px solid #fff;}
-td {border-right:1px solid #f0f0f0;}
-.footer{  background-color: grey; width: auto; max-width: 100%; padding: 0 15px;} #top{ text-align: center} .header{text-align: center}</style>
+  .cancelbtn { width: 100%; }}
+    #top{ text-align: center}</style>
 		<script src="shedding.js"></script>
 </head>
 <body>
@@ -95,14 +68,13 @@ td {border-right:1px solid #f0f0f0;}
             <a href="#" class="nav-item nav-link" onclick="document.getElementById('id01').style.display='block'">Contact Us</a> &nbsp; &nbsp;
              <abbr title="COCIS NEWS"> <a href="https://cocis.news/" style="color:red"> <img src="imgs/cocis.png" style="height: 40px; width: 40px; border-radius: 5px;" alt="COCIS NEWS"></a> </abbr> &nbsp; &nbsp;
             <a href="https://twitter.com/usherTgodwin" class="fa fa-twitter" style=" background: #55ACEE;
-  color: white; font-size: 30px; width: 40px; height: 40px; border-radius: 5px;"></a>&nbsp; &nbsp;
-            <a href="https://www.linkedin.com/in/tumuhimbise-usher-godwin-8947b3189/" class="fa fa-linkedin-square" style="font-size:50px;color:blue; width: 40px; height: 40px;" ></a> &nbsp; &nbsp;
-<a href="https://api.whatsapp.com/send?phone=+256756809525" class="fa fa-whatsapp" style="font-size:45px;color:green; height: 40px;"></a>
-        </div> &nbsp; &nbsp;
+  color: white; font-size: 30px; width: 40px; height: 40px; border-radius: 5px;"></a>
+            <a href="https://www.linkedin.com/in/tumuhimbise-usher-godwin-8947b3189/" class="fa fa-linkedin-square" style="color:blue; font-size: 30px;" ></a> &nbsp; &nbsp;
+<a href="https://api.whatsapp.com/send?phone=+256756809525" class="fa fa-whatsapp" style="color:green; font-size: 30px;"></a>
+        </div> &nbsp;&nbsp;
         <form class="form-inline ml-auto" method="post" action="search-specific.php">
-            <input type="text" class="form-control mr-sm-2" placeholder="Search..." name="parish"><button type="submit" class="btn btn-outline-light" name="submit">Search</button>
-        </form>
-        
+                <input type="text" class="form-control mr-sm-2" placeholder="Search..." name="parish" required><button type="submit" class="btn btn-outline-light" name="submit">Search</button>
+        </form>    
     </div>
 </nav>
     <div class="card">
@@ -125,20 +97,19 @@ window.onclick = function(event) {
     <div class="imgcontainer">
       <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
     </div>
-  <a href="tel:+256756809525">Call us at +256-756-809-525</a> 
-      <p>or send an email</p>
+  <a href="tel:+256756809525"> <i class="fa fa-phone"></i> &nbsp; +256-756-809-525</a> 
+      <p class="text-primary"> <i class="fa fa-envelope"> </i> &nbsp; +256784664867</p> 
     <div class="container">
       <label for="email" class="control-lable"><b>Email</b></label>
  <input type="text" class="form-control" name="EmailAddress" placeholder="someone@example.com" required>
 
       <label for="text" class="control-label"><b>Simple Concern</b></label> <br>
    <textarea cols="40" rows="3" class="form-control"></textarea>
-        <button type="submit" class="btn btn-primary btn-lg" onclick="ValidateEmail(document.form1.EmailAddress)">Send</button>
     </div>
-
-    <div class="container" style="background-color:#f1f1f1">
+          <div class="container">
+    <button type="submit" class="btn btn-primary" onclick="ValidateEmail(document.form1.EmailAddress)">Send</button>
       <button type="button" onclick="document.getElementById('id01').style.display='none'" class="btn btn-outline-danger">Cancel</button>
-    </div>
+      </div>
   </form>
 </div>
 
@@ -167,7 +138,7 @@ window.onclick = function(event) {
       <div class="form-group">
    <input id="input-group" type="text" name="parish" placeholder="Type your location here" title="Search for your location, only Letters allowed" required class="form-control">
       </div>
-   <button type="submit" class="btn btn-primary"> <i class="fa fa-search"></i> Search</button>
+   <button type="submit" class="btn btn-primary"> <i class="fa fa-search"></i> &nbsp;Search</button>
    </form>
              <p class="text-info">Infor: A power outage (also called a power cut, a power out, a power blackout, power failure or a blackout) is the loss of the electrical power network supply to an end user.</p>
          </div>
@@ -204,26 +175,27 @@ window.onclick = function(event) {
     </div>
      </div>
     </div>
- <button class="open-button" onclick="openForm()">Add Location</button>
+    <div class="container">
 <div class="form-popup" id="myForm">
   <form action="index.php" class="form-container" method="POST">
     <?php include("errors.php") ?>
     <h2>Add Location</h2>
 
     <label for="district"><b>District</b></label>
-    <input type="text" placeholder="Enter District" name="district" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $District; ?>">
+    <input type="text" placeholder="Enter District" name="district" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $District; ?>" class="form-control">
 
     <label for="division"><b>Division/ Sub County</b></label>
-    <input type="text" placeholder="Enter Division" name="division" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $Division; ?>">
+    <input type="text" placeholder="Enter Division" name="division" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $Division; ?>" class="form-control">
 
     <label for="parish"><b>Parish/ Town</b></label>
-    <input type="text" placeholder="Enter parish ot town" name="parish" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $Parish; ?>">
+    <input type="text" placeholder="Enter parish ot town" name="parish" required pattern="\w+" title="Enter Only Letters, without spaces" value="<?php echo $Parish; ?>" class="form-control">
 
 
     <button type="submit" class="btn btn-primary btn-sm" name="reg_loc" onclick="return conf()">Submit</button>
-    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+    <button type="button" class="btn btn-danger btn-sm" onclick="closeForm()">Close</button>
   </form>
 </div>
+    </div>
 
 <script>
 function openForm() {
@@ -243,27 +215,18 @@ function conf() {
 }
 </script>
 
- <br> <hr> <br>
-<div class="footer" align="center">
-    <table>
-        <tr>
-            <th colspan="2"> <p style="color: whitesmoke;"> Copyright &copy BIST Group C LoadShedding 2020</p></th> </tr>
-      <tr style="color: blue;">
-    
-    <td ><a href="terms.php">Terms & Conditions</a></td>  <td><a href="#" onclick="openAlert()"> Privacy Policy</a></td> <td ><a href="aboutus.php">About us</a></td>
-    <tr>
-      <br> <br>
-  </table>
-
-
-  <table>
-    
-    <tr style="color: blue;">
-        
-        <td><h4>Load</h4></td> <td><img src="imgs/logo.jpg" width="70" height="50" style="border-radius: 20%"></td> <td><h4>Shedding</h4></td>
-      </tr>
-  </table>
+ <hr> 
+<div class="container">
+    <footer>
+    <div class="row">
+        <div class="col-md-6">
+  <p class="text-muted"> Copyright &copy BIST Group C LoadShedding 2020</p>
+        </div>
+        <div class="col-md-6">
+<a href="terms.php">Terms & Conditions</a> &nbsp;| &nbsp;<a href="#" onclick="openAlert()"> Privacy Policy</a> &nbsp;| &nbsp;<a href="aboutus.php">About us</a> <a href="#" class="btn btn-outline-info" onclick="openForm()">Add Location</a>
+        </div>
   </div>
-
+    </footer>
+    </div>
 </body>
 </html>
