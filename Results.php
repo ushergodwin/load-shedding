1f21e1b09332e7268845e7470c65e9d143f59499
ushergodwin <65310616+ushergodwin@users.noreply.github.com>
2020-05-22 13:14:18 +0100

    Update Results.php

--- Results.php
+++ Results.php
@@ -6,7 +6,6 @@
     <meta name="author" content="Tumuhimbise Usher Godwin">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Current Schedules - ugsheds</title>
-    <link rel="stylesheet" type="text/css" href="results.css">
     <link rel="shortcut icon" type="image/x-icon" href="imgs/favicon.ico">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -14,7 +13,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <style type="text/css"> body{background-color: aliceblue} th{color: blue; text-decoration: underline;}
+    <style type="text/css"> body{background-color: aliceblue}  tr:nth-child(even){
+ 	background-color: #f2f2f2;
+ }th{color: blue; text-decoration: underline;}
     td{ font-family: sans-serif; font-size: 22px;} </style>
 </head>
 <body>
